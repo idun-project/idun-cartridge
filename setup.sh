9983ee1030b3a74036e4900a63a1ed93693025aa
Brian Holdsworth <brian@2c996026-adaf-49ee-8809-d55d424b804e>
2024-02-23 11:32:12 -0500

    FIX: joe configuration for acme

--- setup.sh
+++ setup.sh
@@ -2,11 +2,9 @@
 # Additional packages
 sudo pacman -S acme bastext idun-zcc tmux terminus-font ttf-firacode-nerd
 # Setup 'joe' editor for acme
-if ! [ -f "$HOME/.joe/syntax/acme.jsf" ]; then
-    mkdir -p "$HOME/.joe/syntax"
-    cp /usr/share/joe/syntax/acme.jsf "$HOME/.joe/syntax/acme.jsf"
+if [ -f "/usr/share/joe/syntax/acme.jsf" ]; then
+    sudo mv /usr/share/joe/syntax/acme.jsf /usr/share/joe/syntax/asm.jsf
 fi
-sudo sed -i 's/-syntax asm/-syntax acme/' /etc/joe/joerc
 # Install tmux config and plugins
 mkdir -p "$HOME/.config/tmux"
 if ! [ -f "$HOME/.config/tmux/tmux.conf" ]; then
