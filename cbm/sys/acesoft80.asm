110758dac51c11a0d0dc833480b7acdc13a5cca8
Brian Holdsworth <brian@2c996026-adaf-49ee-8809-d55d424b804e>
2025-05-25 18:05:41 -0400

    Update some comments in soft-80 driver

--- cbm/sys/acesoft80.asm
+++ cbm/sys/acesoft80.asm
@@ -1,9 +1,15 @@
-;ACE-64 kernel VIC bitmapped 80-column screen driver code
-
-;NOTE: this driver _CANNOT_ be used in C128 mode.  It also cannot be compiled
-;      simultaneously with the VDC driver since the 8-bit chrset is under I/O.
-
-;vic memory layout: $c400=chars, $cc00=color, $d000=charset4bit, $e000=bitmap
+; Idun Kernel, Copyright ©2025 Brian Holdsworth
+; This is free software, released under the MIT License.
+;
+; Original version from the ACE-128/64 system,
+; by Craig Bruce, 1992-97 (http://csbruce.com/cbm/ace/)
+;
+; VIC bitmapped 80-column screen driver code
+;
+; NOTE: this driver _CANNOT_ be used in C128 mode.  It also cannot be compiled
+;       simultaneously with the VDC driver since the 8-bit chrset is under I/O.
+;
+; vic memory layout: $c400=chars, $cc00=color, $d000=charset4bit, $e000=bitmap
 
 seCharAddr = $0000  ;logical addr
 seBackAddr = $c400
@@ -922,3 +928,28 @@ seWinOption = *
    clc
    rts
 ++ jmp notImp
+
+;┌────────────────────────────────────────────────────────────────────────┐
+;│                        TERMS OF USE: MIT License                       │
+;├────────────────────────────────────────────────────────────────────────┤
+;│ Copyright (c) 2023 Brian Holdsworth                                    │
+;│                                                                        │
+;│ Permission is hereby granted, free of charge, to any person obtaining  │
+;│ a copy of this software and associated documentation files (the        │
+;│ "Software"), to deal in the Software without restriction, including    │
+;│ without limitation the rights to use, copy, modify, merge, publish,    │
+;│ distribute, sublicense, and/or sell copies of the Software, and to     │
+;│ permit persons to whom the Software is furnished to do so, subject to  │
+;│ the following conditions:                                              │
+;│                                                                        │
+;│ The above copyright notice and this permission notice shall be         │
+;│ included in all copies or substantial portions of the Software.        │
+;│                                                                        │
+;│ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND         │
+;│ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     │
+;│ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. │
+;│ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   │
+;│ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   │
+;│ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      │
+;│ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 │
+;└────────────────────────────────────────────────────────────────────────┘
