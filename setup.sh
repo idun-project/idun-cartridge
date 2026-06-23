74266d2a2efee988001fdef2801e460a61b0bae1
Brian Holdsworth <brian@2c996026-adaf-49ee-8809-d55d424b804e>
2026-06-23 13:32:37 -0400

    Replace tmux with zellij for cartridge development workflow.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 # Additional packages
-sudo apk add acme bastext idun-zcc tmux font-terminus
+sudo apk add acme bastext idun-zcc zellij
 # Setup 'joe' editor for acme
 if [ -f "/usr/share/joe/syntax/acme.jsf" ]; then
     sudo mv /usr/share/joe/syntax/acme.jsf /usr/share/joe/syntax/asm.jsf
-fi
-# Install tmux config and plugins
-mkdir -p "$HOME/.config/tmux"
-if ! [ -f "$HOME/.config/tmux/tmux.conf" ]; then
-    echo "set -g mouse on
-    # List of plugins
-    set -g @plugin 'tmux-plugins/tpm'
-    set -g @plugin 'tmux-plugins/tmux-sensible'
-    #set -g @plugin 'catppuccin/tmux'
-    set -g @plugin 'tmux-plugins/tmux-yank'
-    # Initialize tpm (keep at very bottom of tmux.conf)
-    run '~/.config/tmux/plugins/tpm/tpm'" >> "$HOME/.config/tmux/tmux.conf"
-    mkdir -p "$HOME/.config/tmux/plugins"
-    git clone https://github.com/tmux-plugins/tpm "$HOME/.config/tmux/plugins/tpm"
 fi
